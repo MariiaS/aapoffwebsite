146fddd5368821266e73dfbef70658bef1369edb
mariasolodiankinafi <ext-Mariia.Solodiankina@finnair.com>
2023-10-22 01:03:31 +0300

    Deleted mobile files

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "be8ba959ebe1901ca70b291347fcd6d9",
-"index.html": "50e4eac84e480c2b88ec883c134a2461",
-"/": "50e4eac84e480c2b88ec883c134a2461",
+"index.html": "f378eaf820a6f84670d5d4215c102e78",
+"/": "f378eaf820a6f84670d5d4215c102e78",
 "main.dart.js": "58a318b295c3228482e9072cc8b1d1a4",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "d41d8cd98f00b204e9800998ecf8427e",
