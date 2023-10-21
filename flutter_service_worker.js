d51fc3d5008dd68b775c55de8426168902a95e2b
MariiaS <MariiaS@users.noreply.github.com>
2023-10-21 21:48:09 +0000

    deploy: 0e15c1dd8dd07bbf3d11d7230c9f1c63d01d47bd

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "1175315251f5792683ba5a7936cfa441",
-"/": "1175315251f5792683ba5a7936cfa441",
+const RESOURCES = {"index.html": "0a3830eb278fd7b9f5f895ab7c05aad5",
+"/": "0a3830eb278fd7b9f5f895ab7c05aad5",
 "version.json": "be8ba959ebe1901ca70b291347fcd6d9",
 "favicon.png": "d41d8cd98f00b204e9800998ecf8427e",
 "assets/AssetManifest.bin": "4595b48e19a7267cafe7647224cdb8d5",
